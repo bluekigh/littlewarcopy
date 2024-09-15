2ecde006b1ff6a4a3dc3862e63aa25734d44627d
blueligh <hangaram2@itsnowball.com>
2024-09-15 22:08:04 +0900

    LittleWar init

---
+++ Assets/Script/MyType.cs
@@ -0,0 +1,22 @@
+public class MyType
+{
+
+    public enum Unit
+    {
+        Swrord,
+        Range,
+        Guard,
+        Wizard,
+        bullet
+    }
+
+    public enum AnimationState
+    {
+        doMove,
+        doAttack,
+        doHit,
+        doDie,
+        doStop
+    }
+
+}
